37c04f1c9f03a2c268d34c41e2297908aa7926fc
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-03 07:27:08 +0530

    Navbar added with links and cart icon

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { Button } from '@/components/ui/button';
+import { ShoppingBag } from 'lucide-react';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const links = [
+    { name: 'Home', href: '/' },
+    { name: 'Men', href: '/Men' },
+    { name: 'Women', href: '/Women' },
+    { name: 'Teens', href: '/Teens' },
+];
+
+
+const Navbar = () => {
+    const pathname = usePathname();
+    return (
+        <header className=' mb-8 border-b'>
+            <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+                <Link href='/'>
+                    <h1 className=" text-2xl md:text-4xl font-bold">
+                        <span className=' text-red-700'>NOT </span> <span className=' text-primary'>CHOR BAZAAR </span>
+                    </h1>
+                </Link>
+
+                <nav className=' hidden gap-12 lg:flex 2xl:ml-16'>
+                    {links.map((link, idx) => (
+                        <div key={idx}>
+                            {pathname === link.href ? (
+                                <Link className=' text-lg font-semibold text-primary' href={link.href}>{link.name}
+                                </Link>
+                            ) : (
+                                <Link className=' text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary' href={link.href}>
+                                    {link.name}
+                                </Link>
+                            )}
+                        </div>
+
+                    ))}
+                </nav>
+
+                <div className="flex divide-x border-r sm:border-l">
+                    <Button variant={'outline'} className=' flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none' >
+                        <ShoppingBag  />
+                        <span className=' hidden text-xs font-semibold text-gray-500 sm:block'>Cart</span>
+                    </Button>
+                </div>
+
+
+            </div>
+
+
+        </header>
+    )
+}
+
+export default Navbar
