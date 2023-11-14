3fe766105cb3de8a034e226579e37d19b9e781b8
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-14 14:13:08 +0530

    added icon on stripe error page

--- app/stripe/error/page.tsx
+++ app/stripe/error/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { LucideShieldClose, SquareDashedBottomCodeIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function ErrorStripe() {
     return (
-        <div className=" py-10">
-            <h1>Something went wrong....</h1>
+        <div className=" mt-32 md:max-w-[50vw] mx-auto">
+            <LucideShieldClose className=" text-red-600 w-16 h-16 mx-auto my-6" />
+            <div className=" text-center">
+                <h3 className=" md:text-2xl text-base text-gray-900 font-semibold  text-center ">Oops... Something went wrong!</h3>
+
+                <Button asChild className=" mt-5">
+                    <Link href="/">
+                        Continue Shopping
+                    </Link>
+                </Button>
+            </div>
         </div>
     )
 }
